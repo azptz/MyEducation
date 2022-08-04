61fa6adbf940f167c70c229d6931490d4f7a45ee
azptz <contact@inbox.ru>
2022-08-04 06:24:04 +0300

    1

---
+++ Example001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Data;
+//using Internal;
+using System;
+
+namespace Example001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           /*Console.WriteLine("Как тебя зовут?");
+           string username = Console.ReadLine();
+           Console.WriteLine("Привет,");
+           Console.WriteLine(username);*/
+           int s = new Random().Next(20,80);
+           int a = 5;
+           int b = 54;
+           int result = a+b*s;
+           Console.WriteLine(result);
+        }
+    }
+}
